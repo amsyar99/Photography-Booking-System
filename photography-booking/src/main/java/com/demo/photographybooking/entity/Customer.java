0985c671831db5e14d551cff67f29f927e37cc75
amsyar99 <amsyargy@gmail.com>
2023-06-06 21:15:26 +0800

    change library

--- photography-booking/src/main/java/com/demo/photographybooking/entity/Customer.java
+++ photography-booking/src/main/java/com/demo/photographybooking/entity/Customer.java
@@ -2,7 +2,6 @@ package com.demo.photographybooking.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
--- photography-booking/src/test/java/com/demo/photographybooking/PhotographyBookingApplicationTests.java
+++ photography-booking/src/test/java/com/demo/photographybooking/PhotographyBookingApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PhotographyBookingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
